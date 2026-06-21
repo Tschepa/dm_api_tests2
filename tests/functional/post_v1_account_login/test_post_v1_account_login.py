4c588c6f4a66b1c4ee2aae9dada887e75eb61365
Nina Tsche <cherepanova-nina@yandex.ru>
2026-06-21 19:10:15 +0300

    homework version final

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -9,7 +9,7 @@ def test_v1_account_login():
     account_api = AccountApi(host='http://185.185.143.231:5051')
     login_api = LoginApi(host='http://185.185.143.231:5051')
     mailhog_api = MailhogApi(host='http://185.185.143.231:5025')
-    login = 'wow114'
+    login = 'wow33'
     email = f'{login}@mail.ru'
     password = '12345678'
     
