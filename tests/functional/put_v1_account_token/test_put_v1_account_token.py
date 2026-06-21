c10fa254120130579dfec2795a96cd9fd0b8076f
Nina Tsche <cherepanova-nina@yandex.ru>
2026-06-21 19:13:26 +0300

    last version

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -11,7 +11,7 @@ def test_v1_account_token():
     login_api = LoginApi(host='http://185.185.143.231:5051')
     mailhog_api = MailhogApi(host='http://185.185.143.231:5025')
     
-    login = 'wow30'
+    login = 'wow25111'
     email = f'{login}@mail.ru'
     password = '12345678'
     
