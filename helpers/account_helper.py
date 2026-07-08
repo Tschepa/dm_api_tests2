2b103f809324d41509a492bfb6108e419c695857
Nina Tsche <cherepanova-nina@yandex.ru>
2026-07-08 18:44:46 +0300

    добавили ассерты

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -43,8 +43,9 @@ class AccountHelper:
         self.mailhog = mailhog
         
     def auth_client(self, login: str, password: str):
-        response = self.dm_account_api.login_api.post_v1_account_login(
-            json_data={'login':login, 'password':password}
+        response = self.user_login(
+            login=login,
+            password=password
         )
         token = {"x-dm-auth-token": response.headers["x-dm-auth-token"]}
         self.dm_account_api.account_api.set_headers(token)
@@ -59,8 +60,10 @@ class AccountHelper:
         
         response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
         assert response.status_code == 201, f'Пользователь не был создан, {response.json()}'
-        
+        start_time = time.time()
         token = self.get_token(login=login, token_type="activation")
+        end_time = time.time()
+        assert end_time -  start_time < 3, 'Время ожидания активации превышено'
         assert token is not None, f'Токен для пользователя {login} не был получен'
         response = self.activate_user(token=token)
         assert response.status_code == 200, 'Пользователь не был активирован'
