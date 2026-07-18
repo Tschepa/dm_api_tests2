498e4cce200fbe1aaa2ef34c646170ed39219f96
Nina Tsche <cherepanova-nina@yandex.ru>
2026-07-18 14:47:48 +0300

    ДЗ_coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -41,7 +41,7 @@ options = (
     'user.password',
 )
 
-'''@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def setup_swagger_coverage():
     # Создаем папку принудительно
     os.makedirs("swagger-coverage-output/185.185.143.231_5051", exist_ok=True)
@@ -50,8 +50,8 @@ def setup_swagger_coverage():
     
     yield
     reporter.generate_report()
-    reporter.cleanup_input_files()'''
-@pytest.fixture(scope="session", autouse=True)
+    reporter.cleanup_input_files()
+'''@pytest.fixture(scope="session", autouse=True)
 def setup_swagger_coverage():
     try:
         os.makedirs("swagger-coverage-output/185.185.143.231_5051", exist_ok=True)
@@ -62,7 +62,7 @@ def setup_swagger_coverage():
         reporter.cleanup_input_files()
     except Exception as e:
         print(f"⚠️ Swagger coverage error: {e}")
-        yield  # Тесты продолжаются
+        yield  # Тесты продолжаются'''
 
 @pytest.fixture(scope='session', autouse=True)
 def set_config(request):
